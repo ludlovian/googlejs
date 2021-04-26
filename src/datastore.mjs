7fedbc4be4fa55330af6fa40cb1eade3ace0f3d0
Alan Layng <alan@readersludlow.com>
2021-04-26 10:26:26 +0100

    added datastore

---
+++ src/datastore.mjs
@@ -0,0 +1,82 @@
+import { once } from './util.mjs'
+
+let KEY
+
+export class Table {
+  constructor (kind) {
+    this.kind = kind
+  }
+
+  async * fetch () {
+    const datastore = await getDatastoreAPI()
+    const query = datastore.createQuery(this.kind)
+    for await (const entity of query.runStream()) {
+      yield new Entity(entity)
+    }
+  }
+
+  async fetchAll (options) {
+    const entities = []
+    for await (const entity of this.fetch(options)) {
+      entities.push(entity)
+    }
+    return entities
+  }
+
+  async insert (entities) {
+    const datastore = await getDatastoreAPI()
+    entities = verifyEntities(entities, { kind: this.kind, datastore })
+    await datastore.insert(entities)
+  }
+
+  async upsert (entities) {
+    const datastore = await getDatastoreAPI()
+    entities = verifyEntities(entities, { kind: this.kind, datastore })
+    await datastore.upsert(entities)
+  }
+
+  async delete (entities) {
+    const datastore = await getDatastoreAPI()
+    entities = verifyEntities(entities, { kind: this.kind, datastore })
+    await datastore.delete(entities.map(e => e[datastore.KEY]))
+  }
+}
+
+class Entity {
+  constructor (entity) {
+    Object.assign(this, entity)
+  }
+
+  get key () {
+    const k = this[KEY]
+    return !k ? undefined : 'name' in k ? { name: k.name } : { id: k.id }
+  }
+}
+
+const getDatastoreAPI = once(async function getDatastoreAPI ({
+  credentials = 'credentials.json'
+} = {}) {
+  const { Datastore } = await import('@google-cloud/datastore')
+  if (credentials) {
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = credentials
+  }
+
+  const datastore = new Datastore()
+  KEY = datastore.KEY
+  return datastore
+})
+
+function verifyEntities (arr, { kind, datastore }) {
+  if (!Array.isArray(arr)) arr = [arr]
+  for (const entity of arr) {
+    if (!(datastore.KEY in entity)) {
+      if ('id' in entity) {
+        entity[datastore.KEY] = datastore.key([kind, entity.id])
+        delete entity.id
+      } else {
+        entity[datastore.KEY] = datastore.key([kind])
+      }
+    }
+  }
+  return arr
+}
---
+++ src/index.mjs
@@ -0,0 +1,7 @@
+import * as drive_ from './drive.mjs'
+import * as sheets_ from './sheets.mjs'
+import { Table } from './datastore.mjs'
+
+export const drive = drive_
+export const sheets = sheets_
+export const datastore = { Table }
