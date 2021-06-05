c693e12807ad079462cba6e00cd539976f323a38
Alan Layng <alan@readersludlow.com>
2021-06-05 10:45:06 +0100

    feat:add .parts to SerialDate

--- src/serial-date.mjs
+++ src/serial-date.mjs
@@ -2,6 +2,32 @@ const epochStartInSerial = 25569
 const msInDay = 24 * 60 * 60 * 1000
 
 export default class SerialDate {
+  static fromSerial (n) {
+    return new SerialDate(n)
+  }
+
+  static fromUTCDate (d) {
+    return new SerialDate(d.getTime() / msInDay + epochStartInSerial)
+  }
+
+  static fromParts (parts) {
+    parts = [...parts, 0, 0, 0, 0, 0, 0, 0].slice(0, 7)
+    parts[1]--
+    return SerialDate.fromUTCDate(new Date(Date.UTC(...parts)))
+  }
+
+  static fromLocalDate (d) {
+    return SerialDate.fromParts([
+      d.getFullYear(),
+      d.getMonth() + 1,
+      d.getDate(),
+      d.getHours(),
+      d.getMinutes(),
+      d.getSeconds(),
+      d.getMilliseconds()
+    ])
+  }
+
   constructor (serial) {
     this.serial = serial
     Object.freeze(this)
@@ -11,40 +37,22 @@ export default class SerialDate {
     return new Date((this.serial - epochStartInSerial) * msInDay)
   }
 
-  localDate () {
+  parts () {
     const d = this.utcDate()
-    return new Date(
+    return [
       d.getUTCFullYear(),
-      d.getUTCMonth(),
+      d.getUTCMonth() + 1,
       d.getUTCDate(),
       d.getUTCHours(),
       d.getUTCMinutes(),
       d.getUTCSeconds(),
       d.getUTCMilliseconds()
-    )
+    ]
   }
 
-  static fromSerial (n) {
-    return new SerialDate(n)
-  }
-
-  static fromUTCDate (d) {
-    return new SerialDate(d.getTime() / msInDay + epochStartInSerial)
-  }
-
-  static fromLocalDate (d) {
-    return SerialDate.fromUTCDate(
-      new Date(
-        Date.UTC(
-          d.getFullYear(),
-          d.getMonth(),
-          d.getDate(),
-          d.getHours(),
-          d.getMinutes(),
-          d.getSeconds(),
-          d.getMilliseconds()
-        )
-      )
-    )
+  localDate () {
+    const parts = this.parts()
+    parts[1]--
+    return new Date(...parts)
   }
 }
